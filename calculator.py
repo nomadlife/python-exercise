1e44ee386baad4e779b77b08609b66b23c7b589e
nomadlife <joonwoo3816@gmail.com>
2017-09-25 18:10:20 +0900

    calculator_update:fix_and_upgrade

--- calculator.py
+++ calculator.py
@@ -1,17 +1,30 @@
 from tkinter import *
 from tkinter import ttk
 class Calculator:
-    clac_value = 0.0
+    calc_value = 0.0
     div_trigger = False
     mult_trigger = False
     add_trigger = False
     sub_trigger = False
+    answer_trigger = False
 
     def button_press(self, value):
-        entry_val = self.number_entry.get()
-        entry_val += value
-        self.number_entry.delete(0,"end")
-        self.number_entry.insert(0,entry_val)
+        self.number_entry.configure(state='normal')
+
+        if value == 'AC':
+            self.calc_value = 0
+            self.number_entry.delete(0,"end")
+            self.symbol_entry.delete(0,"end")
+        else:
+            if self.answer_trigger == True:
+                self.number_entry.delete(0,"end")
+                self.answer_trigger = False
+            entry_val = self.number_entry.get()
+            entry_val += value
+            self.number_entry.delete(0,"end")
+            self.number_entry.insert(0,entry_val)
+
+        self.number_entry.configure(state='disabled')
 
     def isfloat(self, str_val):
         try:
@@ -41,11 +54,17 @@ class Calculator:
             else :
                 print("- pressed")
                 self.sub_trigger = True
-            # clear the entry box
+
+            self.number_entry.configure(state='normal')
             self.number_entry.delete(0,"end")
+            self.number_entry.configure(state='disabled')
+
+            self.symbol_entry.configure(state='normal')
+            self.symbol_entry.insert(0,value)
+            self.symbol_entry.configure(state='disabled')
 
     def equal_button_press(self):
-        if self.add_trigger or self.trigger or self.mult_trigger or self.div_trigger:
+        if self.add_trigger or self.sub_trigger or self.mult_trigger or self.div_trigger:
             if self.add_trigger:
                 solution = self.calc_value + float(self.entry_value.get())
             elif self.sub_trigger:
@@ -57,21 +76,37 @@ class Calculator:
 
             print(self.calc_value, " ", float(self.entry_value.get()), " ", solution)
 
+            self.number_entry.configure(state='normal')
             self.number_entry.delete(0,"end")
             self.number_entry.insert(0,solution)
+            self.number_entry.configure(state='disabled')
+
+            self.symbol_entry.configure(state='normal')
+            self.symbol_entry.delete(0,"end")
+            self.symbol_entry.configure(state='disabled')
+
+            self.answer_trigger = True
+
 
     def __init__(self,root):
         self.entry_value = StringVar(root, value="")
+        self.symbol_value = StringVar(root, value="")
 
         root.title("Calculator")
-        root.geometry("430x220")
+        root.geometry("600x220")
         root.resizable(width=False, height=False)
 
         style = ttk.Style()
         style.configure("TButton", font="Serif 15", padding=10)
         style.configure("TEntry",font="Serif 18", padding=10)
-        self.number_entry = ttk.Entry(root, textvariable=self.entry_value,width=50)
-        self.number_entry.grid(row=0,columnspan=4)
+        self.number_entry = ttk.Entry(root, textvariable=self.entry_value,
+                    width=30,state='disabled')
+        self.number_entry.grid(row=0,columnspan=3, stick=E)
+
+        self.symbol_entry = ttk.Entry(root, textvariable=self.symbol_value,
+                    width=10,state='disabled')
+        self.symbol_entry.grid(row=0,column=3,columnspan=1)
+
 
         self.button7=ttk.Button(root, text="7", command=lambda:
         self.button_press('7')).grid(row=1,column=0)
@@ -83,7 +118,7 @@ class Calculator:
         self.button_press('9')).grid(row=1,column=2)
 
         self.button_div=ttk.Button(root, text="/", command=lambda:
-        self.button_press('/')).grid(row=1,column=3)
+        self.math_button_press('/')).grid(row=1,column=3)
 
 
         self.button4=ttk.Button(root, text="4", command=lambda:
@@ -96,7 +131,7 @@ class Calculator:
         self.button_press('6')).grid(row=2,column=2)
 
         self.button_mult=ttk.Button(root, text="*", command=lambda:
-        self.button_press('*')).grid(row=2,column=3)
+        self.math_button_press('*')).grid(row=2,column=3)
 
 
         self.button1=ttk.Button(root, text="1", command=lambda:
@@ -109,7 +144,7 @@ class Calculator:
         self.button_press('3')).grid(row=3,column=2)
 
         self.button_add=ttk.Button(root, text="+", command=lambda:
-        self.button_press('+')).grid(row=3,column=3)
+        self.math_button_press('+')).grid(row=3,column=3)
 
 
         self.button_clear=ttk.Button(root, text="AC", command=lambda:
@@ -119,13 +154,14 @@ class Calculator:
         self.button_press('0')).grid(row=4,column=1)
 
         self.button_equal=ttk.Button(root, text="=", command=lambda:
-        self.button_press()).grid(row=4,column=2)
+        self.equal_button_press()).grid(row=4,column=2)
 
         self.button_sub=ttk.Button(root, text="-", command=lambda:
-        self.button_press('-')).grid(row=4,column=3)
+        self.math_button_press('-')).grid(row=4,column=3)
 
 root = Tk()
 
 clac = Calculator(root)
+
 
 root.mainloop()
