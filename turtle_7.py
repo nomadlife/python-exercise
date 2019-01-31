1aa8a8aa61a7acf5fc498aa1df2743dae69c0d78
nomadlife15u <aaeacd@gmail.com>
2019-01-31 16:46:32 +0900

    turtle race test

---
+++ turtle_7.py
@@ -0,0 +1,29 @@
+# object
+
+import turtle as t
+
+def showTurtle():
+    t.st()
+    return
+
+def getPos(x,y):
+    print("(", x, "," ,y,")")
+    return
+
+def hideTurtle(x,y):
+    t.ht()
+    return
+
+def main():
+    t.speed(20)
+    t.shapesize(1000,1000)
+    t.up()
+    t.goto(1000,0)
+    t.ht()
+    t.onkey(showTurtle,"a")
+    t.listen()
+    t.onscreenclick(getPos)
+    t.onrelease(hideTurtle)
+    t.mainloop()
+
+main()
